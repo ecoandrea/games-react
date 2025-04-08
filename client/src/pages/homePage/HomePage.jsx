ab26dfb909440ecf123cf3a507fd72bbe0042e21
Cristina Zamora <ecoandrea@gmail.com>
2025-04-08 10:30:12 -0400

    feat: backend added, server & create controllers working

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react"
-import { CardJuego } from "./components/CardJuego"
+import { CardGame } from "./components/CardGame"
 
 export const HomePage = () => {
 
-    const [juegos, setJuegos] = useState([]) //si recibe un objeto sria {} si fuera un string seria ""
+    const [games, setGames] = useState([]) //si recibe un objeto sria {} si fuera un string seria ""
 
-    const getJuegos = async() =>{
+    const getGames = async() =>{
         try {
-            const response = await fetch("http://localhost:3000/api/v1/juegos")
+            const response = await fetch("http://localhost:3000/api/v1/games")
             const data = await response.json()
             console.log(data);
-            setJuegos(data.data)
+            setGames(data.data)
         } catch (error) {
             console.log(error);
         }
@@ -18,7 +18,7 @@ export const HomePage = () => {
 
     //useEffect se ejecuta cada vez que se renderiza el componente, en este caso se ejecuta cuando se monta el componente
     useEffect(() => {
-        getJuegos()
+        getGames()
     }, [])
     
 
@@ -28,8 +28,8 @@ export const HomePage = () => {
             <div className="flex space-x-6 mt-5">
                 
                 {
-                    juegos.map((juego) => (
-                        <CardJuego key={juego.id} juego={juego}/>
+                    games.map((game) => (
+                        <CardGame key={game.id} game={game}/>
                     ))
                     //cuando se renderiza con html no se usa  {} en react se usa ()
                     //debe ir key sino da error , ideal que sea unica por eso se usa con .id
--- client/src/pages/homePage/components/CardGame.jsx
+++ client/src/pages/homePage/components/CardGame.jsx
@@ -2,30 +2,30 @@ import { FaGamepad } from "react-icons/fa";
 import { IoLogoGameControllerB } from "react-icons/io";
 import { useState } from "react";
 
-export const CardJuego = ({ juego }) => {
+export const CardGame = ({ game }) => {
     const [fondo, setFondo] = useState("text-sky-800");
 
     return (
         <div
             className={`max-w-sm rounded overflow-hidden shadow-lg bg-white ${
-                juego.consola == "Super Nintendo"
+                game.console == "Super Nintendo"
                     ? "bg-sky-300"
                     : "bg-green-300"
             }`}
         >
-            <img className="w-full" src={juego.imagen} alt={juego.nombre} />
+            <img className="w-full" src={game.image} alt={game.name} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{juego.nombre}</div>
+                <div className="font-bold text-xl mb-2">{game.name}</div>
                 <p className="text-gray-700 text-base">
-                    {juego.consola == "Super Nintendo" ? (
+                    {game.console == "Super Nintendo" ? (
                         <FaGamepad />
                     ) : (
                         <IoLogoGameControllerB />
                     )}
                 </p>
-                <p className={`${fondo} text-base`}>{juego.year}</p>
+                <p className={`${fondo} text-base`}>{game.year}</p>
                 <p className="text-gray-700 text-base mt-2">
-                    {juego.descripcion}
+                    {game.description}
                 </p>
                 <button
                     className="bg-green-400 rounded-2xl mt-4 px-2 py-2 hover:bg-sky-500 hover:scale-105"
